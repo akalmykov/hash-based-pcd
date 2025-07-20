d03fa36afec6c2946bdcafc6f07bd65d242bc825
akalmykov <alexey.kalmykoff@gmail.com>
2025-07-20 03:28:14 +0300

    limit mimc to 54 bits

--- mimc/__main__.py
+++ mimc/__main__.py
@@ -1,9 +1,13 @@
 import argparse
+
 from .mimc_prime_field import mimc_main
+
+# from .mimc_constants_generation import gen_constants
 
 
 def main():
     mimc_main()
+    # gen_constants()
 
 
 if __name__ == "__main__":
